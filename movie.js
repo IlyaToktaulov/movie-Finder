9d73db77fef6ad50564c95798d5c9c4ea4069289
IlyaToktaulov <toktaulovia@mail.ru>
2026-01-28 20:57:22 +0300

    the transition to the movie page has been implemented

--- movie.js
+++ movie.js
@@ -1,4 +1,23 @@
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(location.search);
 const id = urlParams.get('id');
 
-console.log(id);
+fetch(`http://www.omdbapi.com/?i=${id}&apikey=c24b384d`)
+    .then(response => response.json())
+    .then(json => {
+        const filmInfo = `
+            <img class="film-page-poster" src="${json.Poster}" alt='Постер фильма не найден' >
+            <div class="film-page-text-wrapper">
+                <h1 class="film-page-title">${json.Title}</h1>
+                <p class="film-info">Год: <span class="film-info-response">${json.Year}</span></p>
+                <p class="film-info">Рейтинг: <span class="film-info-response">${json.Rated}</span></p>
+                <p class="film-info">Дата выхода: <span class="film-info-response">${json.Released}</span></p>
+                <p class="film-info">Продолжительность: <span class="film-info-response">${json.Runtime}</span></p>
+                <p class="film-info">Жанр: <span class="film-info-response">${json.Genre}</span></p>
+                <p class="film-info">Режисёр: <span class="film-info-response">${json.Director}</span></p>
+                <p class="film-info">Сценарий: <span class="film-info-response">${json.Writer}</span></p>
+                <p class="film-info">Актёры: <span class="film-info-response">${json.Actors}</span></p>
+            </div>
+        `;
+
+        document.querySelector('.js-film-page').innerHTML = filmInfo;
+    })
